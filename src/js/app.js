a2c185870998933ef36f5017be046465e281dc59
Nana <nana.ohl@gmail.com>
2021-01-22 15:44:40 +0000

    Initial

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+// Load jQuery
+window.jQuery = require("./../../../../../wp-includes/js/jquery/jquery");
+
+// Confirm app.js is running
+console.log("app is ready...");
+
+jQuery(document).ready(function ($) {
+  // Confirm jQuery is running
+  console.log("jQuery is ready...");
+
+});
+
